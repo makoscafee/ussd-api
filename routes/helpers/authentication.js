80c415350841d7381da5e88ffb7ce4b9a811cc38
Barnabas Makonda <barnabasmakonda@gmail.com>
2018-03-21 08:55:19 +0300

    handle exception when no user

--- routes/helpers/authentication.js
+++ routes/helpers/authentication.js
@@ -6,15 +6,15 @@ const { generateCode } = require('dhis2-uid');
 export const returnAuthenticationResponse = async (mssdin, sessionid) => {
     let response;
     const { users } = await getUserFromDHIS2(mssdin);
-    response = `You are not allowed to use this system`;
-    if (users) {
-        const dataStore = await getDataStoreFromDHIS2();
-        const { settings, menus } = dataStore;
+    const dataStore = await getDataStoreFromDHIS2();
+    const { settings, menus } = dataStore;
+    response = `C;${sessionid};${settings.no_user_message}`;
+    if (users.length) {
         const starting_menu = menus[settings.starting_menu];
         const name = users[0].displayName;
         response = `P;${sessionid};${`Welcome ${name} to eIDSR Reporting -- Enter PIN`}`;
         if (users.length > 1) {
-            response = `This phone number is associated with more than one number`;
+            response = `C;${sessionid};This phone number is associated with more than one user`;
         } else {
             const id = generateCode();
             const session_data = {
