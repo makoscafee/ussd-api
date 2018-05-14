d339fcd1b133de6858e0654b6a899c584123845b
MAKOSCAFEE <barnabasmakonda@gmail.com>
2018-05-14 16:10:31 +0300

    clean the code

--- endpoints/dataStore.js
+++ endpoints/dataStore.js
@@ -2,7 +2,7 @@ const fetch = require('make-fetch-happen').defaults({
   cacheManager: '../' // path where cache will be written (and read)
 });
 export const getDataStoreFromDHIS2 = async () => {
-  const url = `https://test.hisptz.org/dhis/api/dataStore/ussd/idsr`;
+  const url = `https://test.hisptz.org/dhis/api/dataStore/ussd/idssfr`;
   const Authorization = `Basic Y2hpbmdhbG86Q2hpbmdhbG8xMTE5ODc=`;
 
   const response = await fetch(url, { headers: { Authorization }, size: 0, timeout: 0 });
